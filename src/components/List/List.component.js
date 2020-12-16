3af99a9706872c826fab2b52d274a0a3a566e8ee
Yauheni Bandyukevich <zhenya.bandyukevich@mail.ru>
2020-12-16 22:21:11 +0300

    feat: add new timeSwitcher

--- src/components/List/List.component.js
+++ src/components/List/List.component.js
@@ -1,6 +1,7 @@
 import createElement from "../../utils/createElement";
 import getData from "../../utils/getData";
-import Switcher from "../Switcher/Switcher.component";
+import Switcher from '../Switcher/Switcher.component';
+import SwitcherObserver from '../../SwitcherObserver';
 
 import './List.scss';
 
@@ -14,6 +15,10 @@ function checkPopulationProp(populationProp, population) {
   return 1;
 }
 
+function checkTime(time, TotalConfirmed, NewConfirmed) {
+  return time === 'all time' ? TotalConfirmed : NewConfirmed;
+}
+
 export default class List {
   constructor() {
     this.body = document.querySelector('body');
@@ -22,34 +27,21 @@ export default class List {
     this.listHeadingData =  createElement('div', 'list-heading__data list-heading__data--red');
 
     this.listHeading = createElement('div', 'list-heading', [this.listHeadingTitle, this.listHeadingData]);
-
-    this.listInputAllTime = createElement('input', 'list-switchers__input', null, null, ['type', 'radio'], ['name', 'time'], ['value', 'allTime'], ['checked', 'true']);
-    this.listInputLastDay = createElement('input', 'list-switchers__input', null, null, ['type', 'radio'], ['name', 'time'], ['value', 'lastDay']);
-    this.inputAbsolutePopulation = createElement('input', 'list-switchers__input', null, null, ['type', 'radio'], ['name', 'population'], ['value', 'absolute'], ['checked', 'true']);
-    this.inputRelativePopulation = createElement('input', 'list-switchers__input', null, null, ['type', 'radio'], ['name', 'population'], ['value', 'relative']);
-    
-    this.listSwithers = createElement('div', 'list-switchers', 
-      [createElement('label', 'list-switchers__label', [this.listInputAllTime, 'all time']),
-      createElement('label', 'list-switchers__label', [this.listInputLastDay, 'last day']),
-      createElement('label', 'list-switchers__label', [this.inputAbsolutePopulation, 'all population']),
-      createElement('label', 'list-switchers__label', [this.inputRelativePopulation, 'per 100.000 population']),
-
-    ]);
-
     this.listContent = createElement('ul', 'list__content');
     this.list = createElement('div', 'list', [this.listHeading, this.listSwithers, this.listContent], this.body);
 
+    this.timeSwitcher = new Switcher(this.listHeading, ['all time', 'last day'], (value) => this.updateTime(value));
+
     this.timeProp = 'allTime';
     this.populationProp = 'absolute';
-
-    this.listInputAllTime.addEventListener('change', this.changeTimeProp);
-    this.listInputLastDay.addEventListener('change', this.changeTimeProp);
-
-    this.inputAbsolutePopulation.addEventListener('change', this.changePopulationProp);
-    this.inputRelativePopulation.addEventListener('change', this.changePopulationProp);
     
     this.list.addEventListener('click', this.handleClick);
-    // Switcher(this.list, ['all time', 'last day'], (value) => this.renderHeading(value));
+  }
+
+  updateTime(time) {
+    this.time = time;
+    this.renderHeading();
+    this.renderList();
   }
 
   getPopulation() {
@@ -57,50 +49,30 @@ export default class List {
       this.population = data;
     });
   }
-  
-  changeTimeProp = (event) => {
-    this.timeProp = event.target.value;
-    this.renderList();
-    this.renderHeading();
-  }
-
-  changePopulationProp = (event) => {
-    this.populationProp = event.target.value;
-    this.renderList();
-    this.renderHeading();
-  }
 
   renderHeading(countryName) {
     this.listHeadingData.textContent = '';
-    getData(summaryURL).then((data) => {
+    getData(summaryURL).then(({Countries, Global: {NewConfirmed, TotalConfirmed}}) => {
       if (countryName) {
-        const targetCountry = data.Countries.find((item) => item.Country === countryName);
+        const targetCountry = Countries.find((item) => item.Country === countryName);
         this.listHeadingData.append(`${Math.floor(targetCountry[this.timeProp])}`);
       } else {
-        this.listHeadingData.append(`${data.Global.NewConfirmed}`);
+        this.listHeadingData.append(`${checkTime(this.time, TotalConfirmed, NewConfirmed)}`);
       }
     });
   }
 
   renderList() {
-    let i = 0;
     this.listContent.innerHTML = '';
     getData(summaryURL).then(({Countries}) => {
       Countries.forEach(({Country, TotalConfirmed, NewConfirmed, CountryCode}) => {
         const {population, flag} = this.population.find((item) => item.alpha2Code === CountryCode ) || 0;
-        console.log(i+=1);
         
-        if (this.timeProp === 'allTime') {
-          const listItemFlag = createElement('img', 'list-item__flag', null, null, ['src', `${flag}`]);
-          const countryName = createElement('div', 'list-item__country', Country);
-          const countryData = createElement('div', 'list-item__data', `${Math.floor(TotalConfirmed * checkPopulationProp(this.populationProp, population))}`);
-          const listItem = createElement('li', 'list-item', [listItemFlag, countryName, countryData]);
-          this.listContent.append(listItem);
-        } else {
-          const listItem = createElement('li', 'list-item', 
-            `${Country} ${Math.floor(NewConfirmed * checkPopulationProp(this.populationProp, population))}`, null, ['countryName', Country]);
-          this.listContent.append(listItem);
-        }
+        const listItemFlag = createElement('img', 'list-item__flag', null, null, ['src', `${flag}`]);
+        const countryName = createElement('div', 'list-item__country', Country);
+        const countryData = createElement('div', 'list-item__data', `${Math.floor(checkTime(this.time, TotalConfirmed, NewConfirmed) * checkPopulationProp(this.populationProp, population))}`);
+        const listItem = createElement('li', 'list-item', [listItemFlag, countryName, countryData]);
+        this.listContent.append(listItem);
       });
     });
   }
@@ -115,7 +87,7 @@ export default class List {
 
   init() {
     this.getPopulation();
-    this.renderHeading();
+    // this.renderHeading();
     this.renderList();
   }
 }
